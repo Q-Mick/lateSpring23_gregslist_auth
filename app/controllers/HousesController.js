21de1575e5551d55105491c9e6a103396ccab8f8
Q-Mick <quentinmick@protonmail.com>
2023-05-16 23:22:35 -0600

    testing edit

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -3,7 +3,7 @@ import { housesService } from "../services/HousesService.js";
 import { House } from "../models/House.js";
 import { Pop } from "../utils/Pop.js";
 import { getFormData } from "../utils/FormHandler.js";
-import { setText } from "../utils/Writer.js";
+// import { setText } from "../utils/Writer.js";
 import { AppState } from "../AppState.js";
 
 function _drawHouses(){
@@ -27,6 +27,7 @@ export class HousesController{
   constructor(){
     setHTML('app', "<h1>🏠🛖🏚️🏡🏘️</h1>")
     setHTML('modal-guts', House.HouseForm())
+    setHTML('modal-edit-guts', House.EditHouseForm())
     _drawButton()
     
     AppState.on('account', _drawButton)
@@ -34,6 +35,20 @@ export class HousesController{
     this.getHousesFromApi()
     AppState.on('houses', _drawHouses)
     AppState.on('account', _drawHouses)
+}
+
+async deleteHouse(id){
+  try {
+    const yes = await Pop.confirm("Do you really want to delete the house?")
+    if (!yes){
+      return
+    }
+    // delete the house
+    await housesService.deleteHouse(id)
+
+  } catch (error) {
+    Pop.error(error)
+  }
 }
   async getHousesFromApi() {
     try {
@@ -51,12 +66,34 @@ export class HousesController{
       const form = window.event.target
       const formData = getFormData(form)
       console.log('Form Data ---->', formData)
-      await housesService.CreateHouse(formData)
+      await housesService.createHouse(formData)
+        // @ts-ignore
+      form.reset()
+      // @ts-ignore
+      bootstrap.Modal.getOrCreateInstance('#the-target-id').hide()
     } catch (error) {
       Pop.error(error)
       
     }
   }
-
+  
+  async editHouse(id){
+    try {
+      // @ts-ignore
+      window.event.preventDefault()
+      // @ts-ignore
+      const form = window.event.target
+      const formData = getFormData(form)
+      console.log('Form Data ---->', formData)
+      await housesService.editHouse(formData, id)
+        // @ts-ignore
+      form.reset()
+      // @ts-ignore
+      bootstrap.Modal.getOrCreateInstance('#the-target-id').hide()
+    } catch (error) {
+      Pop.error(error)
+      
+    }
+  }
 
 }
--- app/models/House.js
+++ app/models/House.js
@@ -50,7 +50,8 @@ export class House {
     }
     return /*html*/ `
     <button class="btn btn-danger"
-    onclick="app.HousesController.deleteHouse('${this.id}'">delete</button>`
+    onclick="app.HousesController.deleteHouse('${this.id}')">delete</button>
+    <button onclick="app.HousesController.editHouse('${this.id}')" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#the-edit-id" >Edit</button>`
   }
 
   static HouseForm() {
@@ -101,6 +102,58 @@ export class House {
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="submit" class="btn btn-primary">Create Listing</button>
+      </div>
+    </form>
+    `
+  }
+  static EditHouseForm() {
+    return /*html*/ `
+    <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Listing</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+
+    <form onsubmit="app.HousesController.editHouse()">
+      <div class="modal-body container-fluid">
+        <section class="row">
+          <div class="mb-3 col-4">
+            <label for="make" class="form-label">Bedrooms</label>
+            <input required minlength="1" maxlength="5" type="number" class="form-control" id="bedrooms" name="bedrooms"
+              placeholder="Number of bedrooms">
+          </div>
+          <div class="mb-3 col-4">
+            <label for="bathrooms" class="form-label">Bathrooms</label>
+            <input required minlength="1" maxlength="20" type="number" class="form-control" id="bathrooms" name="bathrooms"
+              placeholder="Bathrooms">
+          </div>
+          <div class="mb-3 col-4">
+            <label for="year" class="form-label">Year</label>
+            <input required min="1920" type="number" class="form-control" id="year" name="year"  placeholder="House Year">
+          </div>
+          <div class="mb-3 col-6">
+            <label for="price" class="form-label">House Price</label>
+            <input required min="2000" max="1000000" type="number" class="form-control" id="price" name="price"
+              placeholder="House Price">
+          </div>
+          <div class="mb-3 col-6">
+            <label for="levels" class="form-label">House Levels</label>
+            <input required min="1" max="1000000" type="number" class="form-control" id="price" name="levels"
+              placeholder="House levels">
+          </div>
+          <div class="mb-3 col-12">
+            <label for="description" class="form-label">House Description</label>
+            <input required minlength="3" maxlength="50" type="text" class="form-control" id="description" name="description"
+              placeholder="house Description">
+          </div>
+          <div class="mb-3 col-12">
+            <label for="imgUrl" class="form-label">House Image</label>
+            <input required type="text" class="form-control" id="imgUrl" name="imgUrl" placeholder="House Image">
+          </div>
+        </section>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Edit Listing</button>
       </div>
     </form>
     `
